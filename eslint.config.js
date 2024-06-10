5b235bb9c5f29a8dfe634a81d012b5a26c846229
Ebenezer Don <ebenezerdonu@gmail.com>
2024-06-10 16:12:18 +0100

    initial setup + appwrite config

---
+++ eslint.config.js
@@ -0,0 +1,7 @@
+import eslintPluginSvelte from 'eslint-plugin-svelte'
+export default [
+  ...eslintPluginSvelte.configs['flat/recommended'],
+  {
+    rules: {},
+  },
+]
---
+++ src/lib/appwrite.js
@@ -0,0 +1,8 @@
+import { Client, Account } from 'appwrite'
+
+export const client = new Client()
+
+client.setEndpoint('https://cloud.appwrite.io/v1').setProject('receiptwise')
+
+export const account = new Account(client)
+export { ID } from 'appwrite'
