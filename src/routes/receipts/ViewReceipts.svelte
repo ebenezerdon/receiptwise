018671ba5c91b2250102c13133595eb2a15495c0
Ebenezer Don <ebenezerdonu@gmail.com>
2024-07-01 13:50:49 +0100

    Update ViewReceipts.svelte to display unique store names

--- src/routes/receipts/ViewReceipts.svelte
+++ src/routes/receipts/ViewReceipts.svelte
@@ -3,18 +3,24 @@
   import { account, db } from '$lib/appwrite';
 
   let receipts = [];
+  let uniqueStores = []
 
   onMount(async () => {
     try {
       await db.receipts.list()
         .then((data) => {
           receipts = data.documents || [];
+          receipts.forEach((receipt) => {
+            uniqueStores.push(receipt.store_name);
+          });
+          uniqueStores = Array.from(new Set(uniqueStores));
         });
-      console.log('====>', receipts);
+      console.log('==receipts==>', receipts);
+      console.log('==uniqueStores==>', Array.from(uniqueStores));
     } catch (error) {
       console.error(error);
     }
-  })
+  });
 </script>
 
 <svelte:head>
@@ -25,9 +31,9 @@
 <section>
   <h1>View Your Receipts</h1>
   <ul>
-    {#each receipts as receipt}
+    {#each uniqueStores as store_name}
       <li>
-        <a href="/receipts/{receipt.$id}">{receipt.store_name}</a>
+        <a href={`/receipts/store/${encodeURIComponent(store_name)}`}>{store_name}</a>
       </li>
     {/each}
   </ul>
